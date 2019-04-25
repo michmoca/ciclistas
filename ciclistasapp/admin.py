7ba881ad16b09cd80e8649185644d2b08cd96a6f
Michael <mich.montes@gmail.com>
2019-04-25 16:44:58 -0600

    mejoras en la grafica tabla ciclista

--- ciclistasapp/admin.py
+++ ciclistasapp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from ciclistasapp.models import  Ciclista, Equipo, CiclistaEquipo, Prueba, PruebasEquipo
 # Register your models here.
 
+class CiclistaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'nacionalidad', 'fecha_nacimiento')
+    list_filter = ('nacionalidad', )
 
-admin.site.register(Ciclista)
+
+
+admin.site.unregister(Group)
+
+admin.site.register(Ciclista, CiclistaAdmin)
 admin.site.register(Equipo)
 admin.site.register(CiclistaEquipo)
 admin.site.register(Prueba)
