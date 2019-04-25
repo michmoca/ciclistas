145a6ddeb9367eeb391e55081e5b03b8d0d11df8
Michael <mich.montes@gmail.com>
2019-04-25 17:25:38 -0600

    Mejoras en las vistas Prueba y Equipo

--- ciclistasapp/admin.py
+++ ciclistasapp/admin.py
@@ -9,11 +9,19 @@ class CiclistaAdmin(admin.ModelAdmin):
     list_filter = ('nacionalidad', )
 
 
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'nacion', 'director')
+    list_filter = ('nacion', )
+
+class EquipoPrueba(admin.ModelAdmin):
+    list_display = ( 'nombre_prueba', 'anio_edicion', 'etapas', 'total_km', 'ciclista_ganador')
+    list_filter = ('anio_edicion', 'total_km',)
+
 
 admin.site.unregister(Group)
 
 admin.site.register(Ciclista, CiclistaAdmin)
-admin.site.register(Equipo)
+admin.site.register(Equipo, EquipoAdmin)
 admin.site.register(CiclistaEquipo)
-admin.site.register(Prueba)
+admin.site.register(Prueba, EquipoPrueba)
 admin.site.register(PruebasEquipo)
